d9ecc6d4cae604b86c5c37ab04ce8f8c111ceeec
maikolravelo82 <maikolravelo82@gmail.com>
2026-05-14 23:58:25 -0500

    kuytorrddsdsadsadsdasdas

--- backend/app.js
+++ backend/app.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 // sincronizar base de datos y levantar servidor
 async function startServer() {
     try {
-        await sequelize.sync({ force: true });
+        await sequelize.sync({ force: false });
         console.log('📦 base de datos sincronizada');
         
         // 🔥 CAMBIO IMPORTANTE: Escuchar en 0.0.0.0
